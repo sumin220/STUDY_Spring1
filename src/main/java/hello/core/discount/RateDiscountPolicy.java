ab5557df58fdc7f57f4645b05fc945856e9d26e9
sonsu <sonsumin00@gmail.com>
2024-07-29 19:24:03 +0900

    feat: order, discount, service

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private final int discountRateAmount = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountRateAmount / 100;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,36 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RateDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 할인은 10퍼센트")
+    void vip_0() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 되어서는 안 된다")
+    void vip_x() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+}
